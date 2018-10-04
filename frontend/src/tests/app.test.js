8d43200efddfe390bf9edc5c3af76614a2c4f8eb
Harry <harryjp1995@gmail.com>
2018-10-04 11:57:15 +0100

    test: Adds first working test

---
+++ frontend/src/tests/app.test.js
@@ -0,0 +1,11 @@
+import "./setupTests";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../components/app";
+
+import { shallow } from "enzyme";
+
+it("renders without crashing", () => {
+  shallow(<App />);
+});
