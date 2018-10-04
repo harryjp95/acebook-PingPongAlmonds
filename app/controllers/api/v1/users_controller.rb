8c2a66c05b5ce1492a0a9c77f8b62b71ea0aa833
Freddie <fredjlawson@gmail.com>
2018-10-04 14:57:50 +0100

    test: adds request specs for user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,33 +1,37 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      def new
-        @user = User.new
-        render json: @user
-      end
+      # def new
+      #   @user = User.new
+      #   render json: @user
+      # end
 
-      def index
-        @users = User.all
-        render json: @users
-      end
+      # def index
+      #   @users = User.all
+      #   render json: @users
+      # end
 
-      def show
-        @user = User.find(params[:id])
-        render json: @user
-      end
+      # def show
+      #   @user = User.find(params[:id])
+      #   render json: @user
+      # end
 
       def create
         @user = User.new(user_params)
         if @user.save
-          render :index
+          json_response(@user, :created) 
         else
-          render @user.errors
+          json_response(@user.errors, :unprocessable_entity)
         end
       end
 
       private
       def user_params
         params.require(:user).permit(:email, :password)
+      end
+
+      def json_response(object, status = :ok)
+        render json: object, status: status
       end
     end
   end
