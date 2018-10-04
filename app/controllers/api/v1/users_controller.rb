ed98ac116697bb914315bfa79f3a69428979f98b
freddie1221 <39989320+freddie1221@users.noreply.github.com>
2018-10-04 15:00:21 +0100

    Remove commented out actions from user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,21 +1,7 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      # def new
-      #   @user = User.new
-      #   render json: @user
-      # end
-
-      # def index
-      #   @users = User.all
-      #   render json: @users
-      # end
-
-      # def show
-      #   @user = User.find(params[:id])
-      #   render json: @user
-      # end
-
+      
       def create
         @user = User.new(user_params)
         if @user.save
