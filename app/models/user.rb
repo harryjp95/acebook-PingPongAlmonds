acff2123763b623c95efd31978723d24c040e54f
Freddie <fredjlawson@gmail.com>
2018-10-05 10:45:27 +0100

    add name & surname

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   acts_as_token_authenticatable
-  attr_accessor :first_name, :surname
+  # attr_accessor :first_name, :surname
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
